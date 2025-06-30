e57d548ba9a41fc1c111eb2ab599f821b97d90b3
Mykal Anderson <mykal.anderson@gmail.com>
2025-06-30 12:03:02 +1000

    Updated menus in base windows.

--- render/qt/base_window.py
+++ render/qt/base_window.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QMainWindow, QMenuBar, QMenu, QApplication, QMessageBox
 from PySide6.QtGui import QAction
+from render.qt.track_editor_window import TrackEditorWindow
 import sys
 
 
@@ -18,9 +19,45 @@ class BaseWindow(QMainWindow):
         file_menu = QMenu("File", self)
         self.menu_bar.addMenu(file_menu)
 
+        # Add New Window action
+        new_window_action = QAction("New Window", self)
+        new_window_action.triggered.connect(lambda: BaseWindow.open_new_window(BlankViewWindow))
+        file_menu.addAction(new_window_action)
+
         exit_action = QAction("Exit", self)
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
+
+
+        # View Menu
+        view_menu = QMenu("View", self)
+        self.menu_bar.addMenu(view_menu)
+
+        from render.qt.track_editor_window import TrackEditorWindow  # near top of file
+
+        # Switch to Blank View
+        blank_view_action = QAction("Blank View", self)
+        from render.qt.blank_view_window import BlankViewWindow
+        blank_view_action.triggered.connect(lambda: self.switch_view(BlankViewWindow))
+        view_menu.addAction(blank_view_action)
+
+        # Switch to Track Editor
+        track_editor_action = QAction("Track Editor", self)
+        track_editor_action.triggered.connect(lambda: self.switch_view(TrackEditorWindow))
+        view_menu.addAction(track_editor_action)
+
+    def switch_view(cls, new_view_class):
+        new_window = new_view_class()
+        new_window.show()
+        cls._open_windows.append(new_window)
+
+        def handle_close(event):
+            cls._open_windows.remove(new_window)
+            if not cls._open_windows:
+                QApplication.instance().quit()
+            super(new_view_class, new_window).closeEvent(event)
+
+        new_window.closeEvent = handle_close
 
     @classmethod
     def open_new_window(cls, window_class):
